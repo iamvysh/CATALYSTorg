7dfba8dfe9077e606f810473831e62468334a374
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-12-20 19:35:13 +0530

    structuring landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import Home from './components/Home/Home'
 import Navbar from './components/navbar/navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
     </>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,18 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div>
+      <img src="/heroPortrait.png" alt="" />
+      <div className="overlay">
+      </div>
+      </div>
+      <div className='homeTexts'>
+      <h1>Pioneering Tomorrow's Digital <br/> Solutions  Today</h1>
+      <h5>UNLEASH THE POWER OF POSSIBILITY</h5>
+      <button>GET IN TOUCH</button>
+      </div>
+    </div>
   )
 }
 
