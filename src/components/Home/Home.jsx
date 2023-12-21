058569411fff3627056064ae182488c6d34b6a4c
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-12-21 15:03:45 +0530

    stars added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   const sliderVariants = {
     initial :{
-      x:0
+      x:'-10%',
     },
     animate:{
       x : '-80%',
@@ -14,7 +14,7 @@ const Home = () => {
       transition : {
         duration : 15,
         repeat : Infinity,
-        repeatType:'mirror',
+        // repeatType:'mirror',
       }
     }
   }
@@ -24,11 +24,21 @@ const Home = () => {
       <div className='wrapper'>
       <div className='image-container'>
       <img src="/heroPortrait.png" alt="" />
-      {/* <div className="overlay"> */}
-      {/* </div> */}
+      <div className="overlay">
+        <motion.div className="star1" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:2, duration:2, repeat:Infinity}}></motion.div>
+        <motion.div className="star2" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:5, duration:3, repeat:Infinity}}></motion.div>
+        <motion.div className="star3" initial={{opacity:1}} animate={{opacity:0}} transition={{duration:2, repeat:Infinity}}></motion.div>
+        <motion.div className="star4" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:2, duration:5, repeat:Infinity}}></motion.div>
+        <motion.div className="star5" initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3, repeat:Infinity}}></motion.div>
+        <motion.div className="star6" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:3, duration:5, repeat:Infinity}}></motion.div>
+        <motion.div className="star7" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:3, duration:4, repeat:Infinity}}></motion.div>
+        <motion.div className="star8" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:2, duration:5, repeat:Infinity}}></motion.div>
+        <motion.div className="star9" initial={{opacity:1}} animate={{opacity:0}} transition={{duration:2, repeat:Infinity}}></motion.div>
+        <motion.div className="star10" initial={{opacity:1}} animate={{opacity:0}} transition={{delay:8,duration:6, repeat:Infinity}}></motion.div>
+      </div>
       </div>
       <div className='homeTexts'>
-      <h1>Pioneering Tomorrow's Digital <br/> Solutions  Today</h1>
+      <h1>Pioneering Tomorrow's <br/> Digital Solutions  Today</h1>
       <h5>UNLEASH THE POWER OF POSSIBILITY</h5>
       <button>GET IN TOUCH</button>
       <motion.div variants={sliderVariants} initial='initial' animate='animate' className='runningText'>CATALYST</motion.div>
