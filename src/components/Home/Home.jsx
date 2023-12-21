1e3b0642db64085a80942dace5adc7a34b02f324
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-12-21 12:27:41 +0530

    framemotion added in homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,38 @@
 import './home.scss'
+import {motion} from 'framer-motion'
 
 const Home = () => {
+
+  const sliderVariants = {
+    initial :{
+      x:0
+    },
+    animate:{
+      x : '-80%',
+      opacity:0,
+
+      transition : {
+        duration : 15,
+        repeat : Infinity,
+        repeatType:'mirror',
+      }
+    }
+  }
+
   return (
     <div className='home'>
-      <div>
+      <div className='wrapper'>
+      <div className='image-container'>
       <img src="/heroPortrait.png" alt="" />
-      <div className="overlay">
-      </div>
+      {/* <div className="overlay"> */}
+      {/* </div> */}
       </div>
       <div className='homeTexts'>
       <h1>Pioneering Tomorrow's Digital <br/> Solutions  Today</h1>
       <h5>UNLEASH THE POWER OF POSSIBILITY</h5>
       <button>GET IN TOUCH</button>
+      <motion.div variants={sliderVariants} initial='initial' animate='animate' className='runningText'>CATALYST</motion.div>
+      </div>
       </div>
     </div>
   )
