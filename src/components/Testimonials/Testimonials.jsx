01cbf000a486cc3fe5fac9c0cdcc45da224b3240
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-12-22 21:37:00 +0530

    corousel added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,8 @@
 import "./Testimonials.scss";
 import { useState } from "react";
+import { IoIosArrowForward , IoIosArrowBack } from "react-icons/io";
+import {motion} from 'framer-motion'
+
 
 const Testimonials = () => {
   const reviewsData = [
@@ -13,8 +16,8 @@ const Testimonials = () => {
     {
       id: 2,
       reviewer: "VYSHNAV",
-      review: `It has been an absolute pleasure working with catalyst! The service provided was top-notch, and I'm thrilled with the results. they demonstrated exceptional professionalism and delivered beyond my expectations. The experience was smooth, and their expertise truly shines through. Highly recommended for anyone seeking reliable and high-quality service!`,
-      company: "BRIDGEON SOLUTIONS",
+      review: `I had a phenomenal experience working with Catalyst. Their team demonstrated unwavering professionalism and an exceptional commitment to delivering top-notch solutions. From the initial consultation to the final implementation, every step was marked by efficiency and expertise. Catalyst not only met but exceeded our expectations, providing innovative and reliable digital solutions that have greatly contributed to our success. I highly recommend Catalyst to anyone seeking a dedicated partner for high-quality services and impactful results.`,
+      company: "DATA SPOT",
       image: "/heroPortrait.png",
     },
   ];
@@ -40,8 +43,12 @@ const Testimonials = () => {
       </h1>
 
       <div className="corousel">
-        <div className="leftArrow" onClick={handlePrevClick}></div>
+        <motion.div whileTap={{rotate:'360deg'}}  className="leftArrow" onClick={handlePrevClick}><IoIosArrowBack/></motion.div>
+        <div className="corouselWrapper">
+        <div className="imageWrapper">
+        <div className="overlay"></div>
         <img src={currentReview.image} alt={currentReview.reviewer} />
+        </div>
         <div className="reviews">
           <div className="reviewContents">
             <h3>{currentReview.reviewer}</h3>
@@ -49,7 +56,8 @@ const Testimonials = () => {
           </div>
            <p>{currentReview.review}</p>
         </div>
-        <div className="rightArrow" onClick={handleNextClick}></div>
+        </div>
+        <motion.div whileTap={{rotate:'360deg'}} className="rightArrow" onClick={handleNextClick}><IoIosArrowForward/></motion.div>
       </div>
     </div>
   );
