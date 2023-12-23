f9c518d9777539ab44a1415ec3959139129d159c
iamvysh <vyshnavthaithottathil@gmail.com>
2023-12-23 10:41:42 +0530

    test 3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
       <motion.h2 initial={{opacity:0}} animate={{opacity:1}} transition={{duration:3,ease:'easeIn'}}>CATAL<span>YS</span>T</motion.h2>
     {/* wrapper */}
       <div className='navigation-wrapper'>
+        {/* sections */}
         <h4>HOME</h4>
         <h4>SERVICES</h4>
         <h4>CONTACT US</h4>
